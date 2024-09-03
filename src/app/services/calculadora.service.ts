d55574517846bb1ba97c087074c2620edc161ab6
EmmanuelAVT9 <alexsoge@gmail.com>
2024-09-03 01:04:18 -0600

    feat(calculator-service): add methods to utilize GET endpoints for arithmetic operations (sumar, restar, multiplicar, dividir, factorial)

--- src/app/services/calculadora.service.ts
+++ src/app/services/calculadora.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculadoraService {
+  //private apiUrl = 'http://localhost:5287/Calculator';
+  private apiUrl = 'http://localhost:5287';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sumar(a: number, b: number): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/sumar?a=${a}&b=${b}`);
+  }
+
+  restar(a: number, b: number): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/restar?a=${a}&b=${b}`);
+  }
+
+  multiplicar(a: number, b: number): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/multiplicar?a=${a}&b=${b}`);
+  }
+
+  dividir(a: number, b: number): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/dividir?a=${a}&b=${b}`);
+  }
+
+  factorial(n: number): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/factorial?n=${n}`);
+  }
 }
